13e5cfdc5ef69577e63a9dc7b75337267534c5a0
noordseoostende <voropayev1@mail.ru>
2021-04-22 20:26:13 +0200

    ik probeer het fout postsSub oplossen en andere model toepassen

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,18 +1,36 @@
-import { Component, Input } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
 
 import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent {
+export class PostListComponent implements OnInit, OnDestroy {
   // posts = [
   //   {title: 'Eerste bericht', content: 'Dat is de eerste bericht\'en van deze content'},
   //   {title: 'Tweede bericht', content: 'Dat is de tweede bericht\'en van deze content'},
   //   {title: 'Derde bericht', content: 'Dat is de derde bericht\'en van deze content'},
   // ]
-  @Input() posts: Post[] = [
+posts: Post[] = [
   ];
+  private postsSub: Subscription;
+
+
+  constructor(public postsService: PostsService) {
+
+  }
+  ngOnInit() {
+    this.posts = this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListener()
+      .subscribe((posts: Post[]) => {
+        this.posts = posts;
+      });
+  }
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
+  }
 }
